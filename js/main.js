98bfe328ea7feceede78d97df00aa722c9d07427
Omar Faruk <40633797+OmarFaruque96@users.noreply.github.com>
2022-12-11 13:40:26 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,106 @@
+// owl carousel js
+$(document).ready(function(){
+
+	$('#products').owlCarousel({
+	    loop:true,
+	    margin:10,
+	    nav:true,
+	    autoplay:true,
+		autoplayTimeout:3000,
+		autoplayHoverPause:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:3
+	        },
+	        1000:{
+	            items:4
+	        }
+	    }
+	});
+
+	$('#bestsellers').owlCarousel({
+	    loop:true,
+	    margin:10,
+	    nav:true,
+	    autoplay:true,
+		autoplayTimeout:3000,
+		autoplayHoverPause:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:3
+	        },
+	        1000:{
+	            items:4
+	        }
+	    }
+	});
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    margin:10,
+	    nav:true,
+	    autoplay:true,
+		autoplayTimeout:3000,
+		autoplayHoverPause:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:1
+	        },
+	        1000:{
+	            items:1
+	        }
+	    }
+	});
+
+	
+
+
+	// carousel next and prev
+	$('.owl-next span').text('Next');
+	$('.owl-prev span').text('Previous');
+
+
+  	const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+	  let today = new Date(),
+	      dd = String(today.getDate()).padStart(2, "0"),
+	      mm = String(today.getMonth() + 1).padStart(2, "0"),
+	      yyyy = today.getFullYear(),
+	      nextYear = yyyy + 1,
+	      dayMonth = "09/30/",
+	      birthday = dayMonth + yyyy;
+	  
+	  today = mm + "/" + dd + "/" + yyyy;
+	  if (today > birthday) {
+	    birthday = dayMonth + nextYear;
+	  }
+  //end
+  
+  const countDown = new Date(birthday).getTime(),
+      x = setInterval(function() {    
+
+        const now = new Date().getTime(),
+              distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+     
+        //seconds
+      }, 0)
+ 
+
+});
